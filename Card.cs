8d2747efad12626290844cc6a7d8baa095517ea6
Deno <denobrockert@yahoo.com>
2019-08-05 11:05:49 -0400

    Refined Validation for card number, expiration date, cvv

--- Card.cs
+++ Card.cs
@@ -24,21 +24,21 @@ namespace MidtermPos
             while (valid)
             {
                 cardNum = Validator.GetValidString("What is the card number?");
-                valid = !Regex.IsMatch(cardNum, @"(([0-9]{4})\-{0,1}([0-9]{4})\-{0,1}([0-9]{4})\-{0,1}([0-9]{4}))");
+                valid = !Regex.IsMatch(cardNum, @"^(([0-9]{4})\-{0,1}([0-9]{4})\-{0,1}([0-9]{4})\-{0,1}([0-9]{4}))$");
             }
             while (!valid)
             {
                 string expDate = Validator.GetValidString("what is the expiration date?");
-                valid = Regex.IsMatch(expDate, @"(([0-1]{1}[0-9]{1})\/([0-9]{2,4}))");
+                valid = Regex.IsMatch(expDate, @"^(([0-1]{1}[0-9]{1})\/([0-9]{2,4}))$");
             }
             while (valid)
             {
                string cvv = Validator.GetCVV("What is the card CVV?");
-                valid = !Regex.IsMatch(cvv, @"([0-9]{3})");
-                if(cvv.Length != 3)
-                {
-                    valid = true;
-                }
+                valid = !Regex.IsMatch(cvv, @"^([0-9]{3})$");
+                //if(cvv.Length != 3)
+                //{
+                //    valid = true;
+                //}
             }
             return $"Card ending in {cardNum.Substring(cardNum.Length - 4, 4)} \n Under {UserName} ${Cost}";
         }
